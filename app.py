5fe3a19b8b99cf33194ceac48aa4890aaa650594
Arnold <aamank1311@gmail.com>
2024-02-08 05:39:39 +0000

    Refactored status code logic for daily records from July 1st to July 7th and added endpoint for /status_codes
    Added '/status_codes' path to the URL endpoint
    
    Corrected status logic for daily records from 01/07 to 07/07
    
    Corrected status logic for daily records from 01/07 to 07/07

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ logfile_path = "NASA_access_log_Jul95"
 # Function to parse the log file and extract requested information
 def parse_logfile(logfile_path):
     ip_counter = Counter()
+<<<<<<< HEAD
+=======
+# status_code_counter = {"2xx": Counter(), "3xx": Counter(), "4xx": Counter(), "5xx": Counter()}
+>>>>>>> b00017b (Refactored status code logic for daily records from July 1st to July 7th and added endpoint for /status_codes)
     date_format = "%d/%b/%Y"
     status_code_counter = defaultdict(lambda: {"2xx": 0, "3xx": 0, "4xx": 0, "5xx": 0})
 
